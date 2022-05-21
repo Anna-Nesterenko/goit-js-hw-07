e013ef3885defb7aff56c8b88dc4754e12b98805
AnnaNesterenko <nura_arh@ukr.net>
2022-05-21 10:12:24 +0300

    finishTask

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -17,15 +17,13 @@ function createGalleryMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
-                <a class="gallery__link" href="${original}">
-                  <img
-                    class="gallery__image"
-                    src="${preview} "
-                    data-source="${original} "
-                    alt="${description} "
-                  />
-                </a>
-              </div>`;
+<a class="gallery__link" href="${original}">
+  <img class="gallery__image"
+   src="${preview} "
+   data-source="${original} "
+   alt="${description} "/>
+</a>
+</div>`;
     })
     .join("");
 }
@@ -62,4 +60,4 @@ function openModal() {
   window.addEventListener("keydown", onCloseModalForEsc);
   getOpanModal();
 }
-// console.log(galleryItems);
+console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const refs = {
+  galleryContainer: document.querySelector(".gallery"),
+};
+
+const cardsMarkup = createGalleryMarkup(galleryItems);
+
+refs.galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img class="gallery__image"
+   src="${preview} "
+   data-source="${original} "
+   alt="${description} "/>
+</a>
+</div>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
